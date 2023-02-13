7df172e5178b40f3162bada9f8f06b6896a95888
vyacheslav <vyacheslav@DESKTOP-DES5PL7>
2023-02-13 17:50:56 +0300

    Add project files.

---
+++ ElectricCircuitEditor/Element.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectricCircuitEditor
+{
+    public class Element
+    {
+        public string Img { get; private set; }
+        public string Name { get; private set; }
+
+        public Element(string img, string name)
+        {
+            Img = img;
+            Name = name;
+        }
+    }
+}
